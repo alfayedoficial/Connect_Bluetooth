9da5ffe33e1e5705aff807a66f2b081d9f9d6305
Ali Al Fayed <alialfayed1@gmail.com>
2023-03-14 14:37:48 +0200

    p-printer

--- app/src/main/java/com/example/connectbluetooth/MainActivity.kt
+++ app/src/main/java/com/example/connectbluetooth/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.BluetoothDevice
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
@@ -22,6 +23,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.connectbluetooth.sample.MainActivity
 import com.example.connectbluetooth.sample.PrinterControl.BixolonPrinter
 import kotlinx.coroutines.*
 import java.io.File
@@ -29,26 +31,6 @@ import java.io.IOException
 import java.util.*
 
 class MainActivity : AppCompatActivity()  , PrinterCallback{
-
-    private lateinit var btnRecord : Button
-    private lateinit var buttonPlay : Button
-    private lateinit var buttonStopSound : Button
-    private lateinit var buttonStop : Button
-    private lateinit var tvCounterRecord1 : TextView
-    var currentTime: Long = 0
-
-    private var isPlaying = false
-    private var isRecording = false
-    private var mAudioRecord: AudioRecord? = null
-    private var mPlayer: MediaPlayer? = null
-    private var mWaveWriter: WaveWriter? = null
-    private var mBufferSize = 0
-
-    private val itemList = ArrayList<MyItem>()
-    private val _adaptor : MyAdapter by lazy {MyAdapter()}
-    private val mHandler by lazy {Handler(Looper.myLooper()!!)}
-    private var mRecordingThread: Thread? = null
-    private var mFileName: String? = null
 
     companion object{
         private var BXL_PRINTER: BixolonPrinter? = null
@@ -70,153 +52,12 @@ class MainActivity : AppCompatActivity()  , PrinterCallback{
         BXL_PRINTER = BixolonPrinter(applicationContext)
 
 
-        findViewById<RecyclerView>(R.id.rvTextCounters).apply {
-            adapter = _adaptor
-            _adaptor.onListSizeChanged ={ size ->
-                if (size == 0) {
-                    // Call API to get data again
-                    Toast.makeText(applicationContext," Call API to get data again" , Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
-        addRandomTimeList()
-        mBufferSize = AudioRecord.getMinBufferSize(44100, 16, 2)
-
-        tvCounterRecord1 = findViewById(R.id.tvCounterRecord1)
-        btnRecord = findViewById(R.id.buttonRecord)
-        buttonStopSound = findViewById(R.id.buttonStopSound)
-        buttonPlay = findViewById(R.id.buttonPlay)
-        buttonStop = findViewById(R.id.buttonStop)
-        btnRecord.setOnClickListener {
-            if (!isRecording) {
-                isRecording = true
-                currentTime = 0
-                tvCounterRecord1.text = "00:00"
-                displayTime()
-                startAudioRecording()
-            }
-
-        }
-
-        buttonStop.setOnClickListener {
-            stopRecord()
-        }
-        buttonPlay.setOnClickListener {
-            if (PlayerServiceUtil.isPlaying()) {
-                PlayerServiceUtil.pause(PauseReason.NONE)
-            }
-            if (PlayerActivity.mMediaPlayer != null) {
-                if (PlayerActivity.mMediaPlayer.isPlaying()) {
-                    PlayerActivity.mMediaPlayer.pause()
-                }
-            }
-            if (!isPlaying) {
-                startAudioPlaying()
-                return
-            } else {
-                pauseAudioPlaying()
-                return
-            }
-        }
-        buttonStopSound.setOnClickListener {
-            stopRecord()
-        }
-
-//        MainScope().launch{
-//            delay(1000)
-//            startActivity(Intent(this@MainActivity, com.example.connectbluetooth.sample.MainActivity::class.java))
-//        }
-    }
-
-    private fun getFilename(): String? {
-        val file: File = MyUtils.ChangeVoiceLocation
-        if (!file.exists()) {
-            file.mkdirs()
-        }
-        return file.absolutePath + "/record.wav"
-    }
-    @SuppressLint("MissingPermission")
-    private fun startAudioRecording() {
-        if (mBufferSize <= 0) {
-            stopRecord()
-            return
-        }
-        deleteMainFile()
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            return
-        }
-        mAudioRecord = AudioRecord(1, 44100, 16, 2, mBufferSize)
-        mFileName = getFilename()
-        mWaveWriter =
-            WaveWriter(File(mFileName), 44100, 1, 16)
-        try {
-            mWaveWriter?.createWaveFile()
-        } catch (exception: IOException) {
-            exception.printStackTrace()
-        }
-        if (mAudioRecord!!.state == 1) {
-            mAudioRecord!!.startRecording()
-            mRecordingThread = Thread { writeAudioDataToFile() }
-            mRecordingThread!!.start()
-            return
-        }
-        Toast.makeText(this , "Error" , Toast.LENGTH_SHORT).show()
-        stopRecord()
-    }
-
-    private fun writeAudioDataToFile() {
-        val sArr = ShortArray(8192)
-        if (mWaveWriter != null) {
-            while (isRecording) {
-                val read = mAudioRecord!!.read(sArr, 0, 8192)
-                if (-3 != read) {
-                    try {
-                        mWaveWriter!!.write(sArr, 0, read)
-                    } catch (exception: IOException) {
-                        exception.printStackTrace()
-                    }
-                }
-            }
+        MainScope().launch{
+            delay(1000)
+            startActivity(Intent(this@MainActivity, MainActivity::class.java))
         }
     }
 
-    fun isEmptyString(str: String?): Boolean {
-        return str == null || str == ""
-    }
-
-    private fun deleteMainFile() {
-        if (!isEmptyString(mFileName)) {
-            try {
-                File(mFileName).delete()
-            } catch (exception: java.lang.Exception) {
-                exception.printStackTrace()
-            }
-        }
-    }
-
-
-    private fun addRandomTimeList() {
-        // Get current time
-        val currentTime = Calendar.getInstance().time
-
-        // Create list to hold MyItems
-
-        // Create random end dates between 1 and 4 minutes from current time
-        val random = Random()
-        for (i in 1..10) {
-            val randomMinute = random.nextInt(2) + 1
-            val randomSec = random.nextInt(10) + 1
-            val endDate = Calendar.getInstance()
-            endDate.time = currentTime
-            endDate.add(Calendar.MINUTE, randomMinute)
-            endDate.add(Calendar.SECOND, randomSec)
-            itemList.add(MyItem("Item $i", endDate.time))
-        }
-
-        _adaptor.add(itemList.toCollection(ArrayList()))
-
-
-    }
 
     @SuppressLint("MissingPermission")
     private fun inflateBluetoothDialog() {
@@ -243,72 +84,6 @@ class MainActivity : AppCompatActivity()  , PrinterCallback{
         MainScope().launch(Dispatchers.Main) {
             Toast.makeText(this@MainActivity, "result is ${result.msg}" , Toast.LENGTH_LONG).show()
         }
-    }
-
-    fun displayTime() {
-        mHandler.postDelayed(java.lang.Runnable {
-            if (currentTime < 60000) {
-                currentTime += 1000
-                var valueOf: String = (currentTime / 1000 / 60).toString()
-                var valueOf2: String = (currentTime / 1000 % 60).toString()
-                if (valueOf.length == 1) {
-                    valueOf = "0$valueOf"
-                }
-                if (valueOf2.length == 1) {
-                    valueOf2 = "0$valueOf2"
-                }
-                tvCounterRecord1.text = "$valueOf:$valueOf2"
-                displayTime()
-                return@Runnable
-            }
-            stopRecord()
-        }, 1000)
-    }
-
-    fun stopRecord() {
-        if (isRecording) {
-            mHandler.removeCallbacksAndMessages(null as Any?)
-            isRecording = false
-            stopAudioRecording(true)
-        }
-    }
-
-    fun stopAudioRecording(b: Boolean) {
-        if (mAudioRecord != null) {
-            try {
-                if (mRecordingThread != null) {
-                    mRecordingThread!!.interrupt()
-                    mRecordingThread = null
-                }
-                mAudioRecord?.stop()
-                mAudioRecord?.release()
-                mAudioRecord = null
-                try {
-                    mWaveWriter?.closeWaveFile()
-                } catch (exception: IOException) {
-                    exception.printStackTrace()
-                }
-            } catch (exception2: java.lang.Exception) {
-                exception2.printStackTrace()
-            }
-        }
-    }
-    private fun pauseAudioPlaying() {
-        var mediaPlayer: MediaPlayer ? = null
-        if (isPlaying && mPlayer?.also { mediaPlayer = it } != null  && mediaPlayer?.isPlaying == true) {
-            mPlayer?.pause()
-            isPlaying = false
-            btnRecord.text = "Record"
-        }
-    }
-
-
-    fun stopAudioPlaying() {
-        if (mPlayer != null) {
-            mPlayer?.release()
-            mPlayer = null
-        }
-        isPlaying = false
     }
 
 }
